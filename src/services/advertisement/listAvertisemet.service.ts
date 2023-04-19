7f98f54a60641d88e4c0b61f8ade40f9f4da2405
unknown <dreicleal@hotmail.com>
2023-04-19 15:24:36 -0300

    feat: advertise owner middleware

--- src/services/advertisement/listAvertisemet.service.ts
+++ src/services/advertisement/listAvertisemet.service.ts
@@ -1,7 +1,6 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../../data-source";
 import { Advertisements } from "../../entities/advertisement.entity";
-import { ListAdvertisementsSchema } from "../../schemas/advertisement";
 
 const ListAdvertisementService = async () => {
   const advertisementRepository: Repository<Advertisements> =
