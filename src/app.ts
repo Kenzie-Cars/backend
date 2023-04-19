227b511b55a8004418a65e677023e8e656e41adf
unknown <dreicleal@hotmail.com>
2023-04-19 07:21:37 -0300

    feat: login

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import express, { Application } from "express";
 import handleError from "./errors";
 import advertisementsRouter from "./routes/advertisement.routes";
 import { userRoutes } from "./routes/user.routes";
+import { sessionRoute } from "./routes/session.routes";
 
 
 export const app: Application = express();
 app.use(express.json())
 app.use("/advertisements", advertisementsRouter);
 app.use("/users", userRoutes);
+app.use("/login", sessionRoute);
 app.use(handleError)
