4e2c4b572a9fe650c60b8c2419a4d9060efb40b8
unknown <dreicleal@hotmail.com>
2023-04-20 08:52:31 -0300

    user serializer

--- src/schemas/users/users.serializers.ts
+++ src/schemas/users/users.serializers.ts
@@ -1,8 +1,24 @@
-import { IUserResponse } from "../../interfaces/users";
+import { IUser, IUserResponse } from "../../interfaces/users";
 import { SchemaOf } from "yup";
 import * as yup from "yup";
 import { addressSerializerResponse } from "../address/address.serializer";
 import { ListAdvertisementsSchema } from "../advertisement";
+
+export const userSerializer: SchemaOf<IUser> = yup.object().shape({
+    email: yup.string().email(),
+    password: yup.string().required().min(4),
+    cpf: yup.string().min(11),
+    phone: yup.string(),
+    birthDate: yup.string().min(8),
+    description: yup.string(),
+    is_adm: yup.boolean().notRequired(),
+    is_seller: yup.boolean(),
+    is_active: yup.boolean(),
+    created_at: yup.date(),
+    updated_at: yup.date(),
+    address: addressSerializerResponse,
+    userAdvertisements: ListAdvertisementsSchema.notRequired()
+}).clone() as SchemaOf<IUser>
 
 export const responseUserSerializer: SchemaOf<IUserResponse> = yup.object().shape({
     id: yup.string(),
